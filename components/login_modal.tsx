efa7c3e6e4f0ef47812f18c7a7ba7306018f01e6
hwuiinn <gnldls8406@gmail.com>
2023-02-21 03:20:30 +0900

    ✨ Feat : Kakao Login, Search #27
    - 카카오 로그인 기능 구현합니다.
    - 검색 기능만 구현합니다.
    - 로그인, 회원가입 디자인 수정합니다.
    - 헤더 로그인/회원가입 버튼 로그아웃 버그 수정합니다.
    
    by @Huwiinn

--- components/login_modal.tsx
+++ components/login_modal.tsx
@@ -1,11 +1,23 @@
-import { authService, dbService } from "@/firebase";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+  authService,
+  dbService,
+  providerFacebook,
+  providerGoogle,
+} from "@/firebase";
+import {
+  FacebookAuthProvider,
+  GoogleAuthProvider,
+  signInWithCustomToken,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
 import React, { SetStateAction, useEffect, useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { GrFacebook } from "react-icons/gr";
-import { SiNaver } from "react-icons/si";
 import { MdOutlineClose } from "react-icons/md";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
+import { SiNaver, SiKakaotalk } from "react-icons/si";
+import axios from "axios";
 
 const LoginModal = ({ isOpen, setIsOpen, setJoinIsOpen }: any) => {
   const [email, setEmail] = useState("");
@@ -32,11 +44,139 @@ const LoginModal = ({ isOpen, setIsOpen, setJoinIsOpen }: any) => {
     if (email) {
       if (email.match(emailRegEx) === null) {
         setEmail("이메일 형식을 확인해주세요.");
-      } else {
-        setEmail("");
       }
     }
-  }, [email, setEmail]);
+  }, [setEmail]);
+
+  console.log("email", email);
+  console.log("password", password);
+
+  // 간편 로그인
+  // 구글 -> uid 생성 후, setDoc으로 document 생성하여 유저 추가.
+  const googleJoin = () => {
+    signInWithPopup(authService, providerGoogle)
+      .then((result: any) => {
+        // 다음은 구글 액세스 토큰을 발급하는 코드입니다. 이 토큰을 사용하여 구글 API에 접근할 수 있습니다.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        setDoc(doc(dbService, "Users", `${authService.currentUser?.uid}`), {
+          userId: authService.currentUser?.uid,
+          email: token, // 실제 값은 이메일이 아닌 token으로 변경.
+          nickname: result.user.displayName,
+          imageURL: "",
+          introduce: "",
+          point: "",
+          following: [],
+          follower: [],
+          recently: [],
+        });
+        // 로그인한 사용자 정보가 제공됩니다.
+        const user = result.user;
+        // 추가 정보는 getAdditionalUserInfo(result)를 사용하여 사용할 수 있습니다.
+        setIsOpen(false);
+        console.log("result : ", result);
+      })
+      .catch((error) => {
+        // 이 부분에서는 오류를 처리합니다.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // 사용된 사용자 계정 이메일
+        const email = error.customData.email;
+        // AuthCredential 타입 제공됩니다.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        console.log("error : ", error);
+      });
+  };
+
+  // 페이스북
+  const facebookJoin = () => {
+    signInWithPopup(authService, providerFacebook)
+      .then((result) => {
+        // 다음은 구글 액세스 토큰을 발급하는 코드입니다. 이 토큰을 사용하여 구글 API에 접근할 수 있습니다.
+        const credential = FacebookAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        setDoc(doc(dbService, "Users", `${authService.currentUser?.uid}`), {
+          userId: authService.currentUser?.uid, // authService가 아닌 token값 중에 하나로 변경해야함. (구글도)
+          email: token, // 실제 값은 이메일이 아닌 token으로 변경.
+          nickname: result.user.displayName,
+          imageURL: "",
+          introduce: "",
+          point: "",
+          following: [],
+          follower: [],
+          recently: [],
+        });
+        // 로그인한 사용자 정보가 제공됩니다.
+        const user = result.user;
+        // 추가 정보는 getAdditionalUserInfo(result)를 사용하여 사용할 수 있습니다.
+        setIsOpen(false);
+        console.log("result : ", result);
+      })
+      .catch((error) => {
+        // 이 부분에서는 오류를 처리합니다.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // 사용된 사용자 계정 이메일
+        const email = error.customData.email;
+        // AuthCredential 타입 제공됩니다.
+        const credential = FacebookAuthProvider.credentialFromError(error);
+        alert(error);
+      });
+  };
+
+  // 카카오 로그인
+  const loginFormWithKakao = () => {
+    window.Kakao.Auth.login({
+      success(authObj: any) {
+        console.log("authObj : ", authObj);
+        window.localStorage.setItem("token", authObj.access_token);
+        axios({
+          method: "POST",
+          url: "http://localhost:3000/api/kakao",
+          data: { authObj },
+        }).then(function (response: any) {
+          // 서버에서 보낸 jwt토큰을 받음
+          console.log(response);
+          localStorage.setItem("data", JSON.stringify(response.data));
+          console.log("responseData", response.data);
+
+          return signInWithCustomToken(
+            authService,
+            `${response.data.firebaseToken}`
+          )
+            .then((userCredential: any) => {
+              const user = userCredential.user;
+              console.log("user : ", user);
+              setDoc(
+                doc(dbService, "Users", authService.currentUser?.uid as string),
+                {
+                  userId: authService.currentUser?.uid,
+                  email: "",
+                  nickname: "카카오",
+                  imageURL: "",
+                  introduce: "",
+                  point: "",
+                  following: [],
+                  follower: [],
+                  recently: [],
+                  alarm: [],
+                }
+              );
+              alert("카카오 간편 회원가입 성공!");
+              setIsOpen(false);
+            })
+            .catch((error: any) => {
+              const errorCode = error.code;
+              const errorMessage = error.message;
+              alert(errorMessage);
+            });
+        });
+      },
+      fail(err: any) {
+        console.log("err :", err);
+      },
+    });
+  };
 
   return (
     <>
@@ -58,9 +198,7 @@ const LoginModal = ({ isOpen, setIsOpen, setJoinIsOpen }: any) => {
                 placeholder="이메일을 입력해주세요."
                 className="w-[472px] h-[44px] p-2 pl-4 mb-1 bg-[#F5F5F5] placeholder:text-[#666]  duration-300 focus:scale-105"
               />
-              <p className="w-[472px] m-auto mb-3 text-right text-sm text-[#999999]">
-                {email ? email : null}
-              </p>
+              <p className="w-[472px] m-auto mb-3 text-right text-sm text-[#999999]"></p>
             </div>
             <div>
               <p className="w-[472px] m-auto mb-[6px] text-left">비밀번호</p>
@@ -90,17 +228,23 @@ const LoginModal = ({ isOpen, setIsOpen, setJoinIsOpen }: any) => {
               </button>
             </div>
 
-            <p className="text-2xl font-bold mb-12 mt-[52px]">간편로그인</p>
+            <p className="text-2xl font-bold mb-12 mt-[52px]">
+              소셜계정으로 로그인
+            </p>
 
-            <div className="w-[473px] m-auto mb-[100px] flex items-center justify-center">
-              <div>
+            <div className="w-[280px] m-auto mb-[24px] flex items-center  justify-around">
+              <div onClick={googleJoin}>
                 <FcGoogle className="w-10 h-10 border bg-black cursor-pointer" />
               </div>
-              <div>
-                <GrFacebook className="w-10 h-10 ml-20 mr-20 border border-slate-400 cursor-pointer" />
+              <div onClick={facebookJoin}>
+                <GrFacebook className="w-10 h-10 ml-10 mr-10 border border-slate-400 cursor-pointer" />
               </div>
-              <div>
+              {/* 네이버 로그인 구현 전 */}
+              {/* <div>
                 <SiNaver className="w-10 h-10 border border-slate-400 cursor-pointer" />
+              </div> */}
+              <div onClick={loginFormWithKakao}>
+                <SiKakaotalk className="w-10 h-10 border bg-white cursor-pointer" />
               </div>
             </div>
             <div className="w-[473px] m-auto flex justify-center text-sm">
--- pages/index.tsx
+++ pages/index.tsx
@@ -16,6 +16,8 @@ import Banner from "@/components/main_page/banner";
 import PostList from "@/components/main_page/post_list";
 
 const Home = () => {
+  console.log("authService : ", authService.currentUser?.uid);
+
   return (
     <Layout>
       <Banner />
