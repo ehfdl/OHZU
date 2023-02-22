5258873ab07e9ef9863028a080aade51e0d0ca1b
HANUL KIM <aouoa03@gmail.com>
2023-02-22 18:42:22 +0900

    ⚡️ Feat: Change banner to swiper
    배너를 swiper로 변경합니다.
    
    by @HanuKim

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
 import Layout from "@/components/layout";
 import React from "react";
-
+import MainBanner from "@/components/main_page/banner";
 import "tailwindcss/tailwind.css";
-import Banner from "@/components/main_page/banner";
 import PostList from "@/components/main_page/post_list";
 
 const Home = () => {
   return (
     <Layout>
-      <Banner />
+      <MainBanner />
       <PostList />
     </Layout>
   );
