b7cda791150428428cb0de41c2283635a671be66
HANUL KIM <aouoa03@gmail.com>
2023-02-22 09:34:50 +0900

    ✨ Feat: Add showmore button #61
    더보기 버튼을 추가합니다.
    
    by @HanuKim

--- components/write_btn.tsx
+++ components/write_btn.tsx
@@ -8,13 +8,13 @@ const WriteButton = () => {
       {/* {showButton && ( */}
       <div className="scroll__container fixed right-[5%] bottom-[5%] z-50">
         <button
-          className="rounded-full w-16 h-16 font-thin text-sm text-white/80 bg-black hover:text-white hover:bg-[#ff6161]/90 hover:shadow-[3px_5px_8px_#ff9999]"
+          className="rounded-full w-16 h-16 font-thin text-sm  text-white bg-[#ff6161]/90 hover:shadow-[3px_5px_8px_#ff9999]  "
           id="write"
           // onClick={scrollToTop}
           type="button"
         >
           <img
-            src="/write/write-default.png"
+            src="/write/write.svg"
             className="ml-5 cursor-pointer hover:fill-[#ff6161]"
           />
         </button>
