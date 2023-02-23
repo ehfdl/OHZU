7561d0233b3f2b98b0a551b012dfa95966b62c56
ehfdl <cmaw@naver.com>
2023-02-23 15:45:43 +0900

    🐛 Fix: join
    join 시 회원이면 재가입 막습니다.
    
    by @ehfdl

--- components/login_modal.tsx
+++ components/login_modal.tsx
@@ -152,7 +152,7 @@ const LoginModal = ({ isOpen, setIsOpen, setJoinIsOpen }: any) => {
         window.localStorage.setItem("token", authObj.access_token);
         axios({
           method: "POST",
-          url: "http://localhost:3000/api/kakao",
+          url: "https://ohzu-8s9obgcwg-ehfdl.vercel.app/api/kakao",
           data: { authObj },
         }).then(function (response: any) {
           // 서버에서 보낸 jwt토큰을 받음
