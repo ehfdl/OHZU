9f8e47010e83d16ef27caf3a1370399c9baf76a4
ehfdl <cmaw@naver.com>
2023-02-24 10:23:15 +0900

    🐛 Fix:modal
    modal backdrop blur 처리합니다.
    modal on 시 스크롤 막습니다.
    로그아웃시 세션제거 처리합니다.
    
    by @ehfdl

--- components/login_modal.tsx
+++ components/login_modal.tsx
@@ -235,88 +235,109 @@ const LoginModal = ({ isOpen, setIsOpen, setJoinIsOpen }: any) => {
     });
   };
 
+  useEffect(() => {
+    document.body.style.cssText = `
+      position: fixed;
+      top: -${window.scrollY}px;
+      overflow-y: scroll;
+      width: 100%;`;
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = "";
+      window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
+    };
+  }, []);
+
   return (
     <>
-      <div className="w-screen h-screen fixed bg-slate-500 z-[9] opacity-90"></div>
-      <div className="inner w-[588px] h-[820px] bg-white z-[10] fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <div className="loginContainer flex-col text-center">
-          <MdOutlineClose
-            onClick={() => setIsOpen(false)}
-            className="absolute top-[32px] right-[32px] w-6 h-6 cursor-pointer duration-150 hover:text-red-400"
-          />
-          <h4 className="text-[40px] font-bold mt-[64px] mb-[42px]">로그인</h4>
-          <form className="formContainer" onSubmit={signIn}>
-            <div>
-              <p className="w-[472px] m-auto mb-[6px] text-left">이메일</p>
-              <input
-                onChange={(e) => setEmail(e.target.value)}
-                type="text"
-                id="email"
-                placeholder="이메일을 입력해주세요."
-                className="w-[472px] h-[44px] p-2 pl-4 mb-1 outline-none bg-[#F5F5F5] placeholder:text-[#666]  duration-300 focus:scale-[1.01]"
-              />
-              <p className="w-[472px] m-auto mb-3 text-right text-sm text-[#999999]"></p>
-            </div>
-            <div>
-              <p className="w-[472px] m-auto mb-[6px] text-left">비밀번호</p>
-              <input
-                onChange={(e) => setPassword(e.target.value)}
-                type="password"
-                id="password"
-                placeholder="비밀번호를 입력해주세요."
-                className="w-[472px] h-[44px] p-2 pl-4 mb-3 outline-none bg-[#F5F5F5] placeholder:text-[#666]  duration-300 focus:scale-[1.01]"
-              />
-              <p className=" w-[472px] m-auto text-right text-gray-500 text-sm cursor-pointer duration-150 hover:text-primary">
-                비밀번호 찾기
+      <div className=" w-full h-screen flex absolute justify-center top-0 left-0 items-center ">
+        <div className="w-full h-full fixed left-0 top-0 z-[9] bg-[rgba(0,0,0,0.5)] backdrop-blur-[2px]" />
+        <div className="inner w-[588px] h-[820px] bg-white z-[10] fixed top-1/2 left-1/2 rounded transform -translate-x-1/2 -translate-y-1/2">
+          <div className="loginContainer flex-col text-center">
+            <MdOutlineClose
+              onClick={() => setIsOpen(false)}
+              className="absolute top-[32px] right-[32px] w-6 h-6 cursor-pointer duration-150 hover:text-red-400"
+            />
+            <h4 className="text-[40px] font-bold mt-[64px] mb-[42px]">
+              로그인
+            </h4>
+            <form className="formContainer" onSubmit={signIn}>
+              <div>
+                <p className="w-[472px] m-auto mb-[6px] text-left">이메일</p>
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="text"
+                  id="email"
+                  placeholder="이메일을 입력해주세요."
+                  className="w-[472px] h-[44px] p-2 pl-4 mb-1 outline-none bg-[#F5F5F5] placeholder:text-[#666]  duration-300 focus:scale-[1.01]"
+                />
+                <p className="w-[472px] m-auto mb-3 text-right text-sm text-[#999999]"></p>
+              </div>
+              <div>
+                <p className="w-[472px] m-auto mb-[6px] text-left">비밀번호</p>
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  id="password"
+                  placeholder="비밀번호를 입력해주세요."
+                  className="w-[472px] h-[44px] p-2 pl-4 mb-3 outline-none bg-[#F5F5F5] placeholder:text-[#666]  duration-300 focus:scale-[1.01]"
+                />
+                <p className=" w-[472px] m-auto text-right text-gray-500 text-sm cursor-pointer duration-150 hover:text-primary">
+                  비밀번호 찾기
+                </p>
+                <div className="flex w-[472px] m-auto">
+                  <label
+                    htmlFor="auto_login"
+                    className="flex  items-center mb-[48px]"
+                  >
+                    <input
+                      id="auto_login"
+                      type="checkbox"
+                      className="w-5 h-5"
+                    />
+                    <span className="ml-2 ">자동 로그인</span>
+                  </label>
+                </div>
+              </div>
+              <div className="buttonWrap mb-4">
+                <button className="w-[280px] h-[48px] mb-[29px] bg-primary text-white rounded   ">
+                  로그인
+                </button>
+              </div>
+
+              <p className="text-2xl font-bold mb-12 mt-[52px]">
+                소셜계정으로 로그인
               </p>
-              <div className="flex w-[472px] m-auto">
-                <label
-                  htmlFor="auto_login"
-                  className="flex  items-center mb-[48px]"
-                >
-                  <input id="auto_login" type="checkbox" className="w-5 h-5" />
-                  <span className="ml-2 ">자동 로그인</span>
-                </label>
-              </div>
-            </div>
-            <div className="buttonWrap mb-4">
-              <button className="w-[280px] h-[48px] mb-[29px] bg-primary text-white rounded   ">
-                로그인
-              </button>
-            </div>
 
-            <p className="text-2xl font-bold mb-12 mt-[52px]">
-              소셜계정으로 로그인
-            </p>
-
-            <div className="w-[280px] m-auto mb-[24px] flex items-center  justify-around">
-              <div onClick={googleJoin}>
-                <FcGoogle className="w-10 h-10 border bg-black cursor-pointer" />
-              </div>
-              <div onClick={facebookJoin}>
-                <GrFacebook className="w-10 h-10 ml-10 mr-10 border border-slate-400 cursor-pointer" />
-              </div>
-              {/* 네이버 로그인 구현 전 */}
-              {/* <div>
+              <div className="w-[280px] m-auto mb-[24px] flex items-center  justify-around">
+                <div onClick={googleJoin}>
+                  <FcGoogle className="w-10 h-10 border bg-black cursor-pointer" />
+                </div>
+                <div onClick={facebookJoin}>
+                  <GrFacebook className="w-10 h-10 ml-10 mr-10 border border-slate-400 cursor-pointer" />
+                </div>
+                {/* 네이버 로그인 구현 전 */}
+                {/* <div>
                 <SiNaver className="w-10 h-10 border border-slate-400 cursor-pointer" />
               </div> */}
-              <div onClick={loginFormWithKakao}>
-                <SiKakaotalk className="w-10 h-10 border bg-white cursor-pointer" />
+                <div onClick={loginFormWithKakao}>
+                  <SiKakaotalk className="w-10 h-10 border bg-white cursor-pointer" />
+                </div>
               </div>
-            </div>
-            <div className="w-[473px] m-auto flex justify-center text-sm">
-              <p className="text-slate-400 mr-1">아직 회원이 아니신가요?</p>
-              <span
-                onClick={() => {
-                  setIsOpen(false);
-                  setJoinIsOpen(true);
-                }}
-                className="cursor-pointer"
-              >
-                회원가입
-              </span>
-            </div>
-          </form>
+              <div className="w-[473px] m-auto flex justify-center text-sm">
+                <p className="text-slate-400 mr-1">아직 회원이 아니신가요?</p>
+                <span
+                  onClick={() => {
+                    setIsOpen(false);
+                    setJoinIsOpen(true);
+                  }}
+                  className="cursor-pointer"
+                >
+                  회원가입
+                </span>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </>
