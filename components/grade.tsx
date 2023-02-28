86bc7d1c1e5beb5c7fb8664c9e82ccc8c011456b
ehfdl <cmaw@naver.com>
2023-02-28 16:44:57 +0900

    💄 Design:mypage
    mobile-UI 반응형 추가합니다.
    grade 추가합니다.
    
    by @ehfdl

--- components/grade.tsx
+++ components/grade.tsx
@@ -1,18 +1,43 @@
+import Image from "next/image";
 import React from "react";
 
 const Grade = ({ score }: { score: number }) => {
   if (score < 21) {
     const rank = "/badge/badge-bronze.png";
 
-    return <img src={rank} />;
+    return (
+      <Image
+        className="w-3 h-[15px] sm:w-[18px] sm:h-[22px]"
+        src={rank}
+        width={12}
+        height={15}
+        alt=""
+      />
+    );
   } else if (score < 31) {
     const rank = "/badge/badge-silver.png";
 
-    return <img src={rank} />;
+    return (
+      <Image
+        className="w-3 h-[15px] sm:w-[18px] sm:h-[22px]"
+        src={rank}
+        width={12}
+        height={15}
+        alt=""
+      />
+    );
   } else {
     const rank = "/badge/badge-gold.png";
 
-    return <img src={rank} />;
+    return (
+      <Image
+        className="w-3 h-[15px] sm:w-[18px] sm:h-[22px]"
+        src={rank}
+        width={12}
+        height={15}
+        alt=""
+      />
+    );
   }
 };
 
