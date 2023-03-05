c9fe66c528c1593955584aa37866f90a501d8a64
hwuiinn <gnldls8406@gmail.com>
2023-03-05 23:36:05 +0900

    ✨ Feat : landing page, loading animation
    - 랜딩페이지 1차 완성입니다.
    - 로딩페이지 완성합니다.
    
    by @Huwiinn

--- components/main_page/post_list.tsx
+++ components/main_page/post_list.tsx
@@ -129,208 +129,210 @@ const PostList = () => {
   const [slicenumber, setSliceNumber] = useState(0);
 
   return (
-    <div>
-      <div className="sm:w-full flex flex-col justify-center items-center sm:mb-3 sm:min-h-screen">
-        <Category setCate={setCate} />
-        <div className="sm:w-[1200px] w-full flex flex-col justify-start items-center px-4">
-          <div className="w-full sm:mt-12 mt-4 mb-4 flex justify-between sm:mb-6">
-            <div className="sm:text-xl text-sm font-bold">
-              게시물{" "}
-              <span className="float-right ml-2 sm:text-xl text-sm font-bold text-primary">
-                {cate === "전체"
-                  ? posts?.length
-                  : posts?.filter((post) => cate === post.type).length}
-              </span>
+    <>
+      <div>
+        <div className="sm:w-full flex flex-col justify-center items-center sm:mb-3 sm:min-h-screen">
+          <Category setCate={setCate} />
+          <div className="sm:w-[1200px] w-full flex flex-col justify-start items-center px-4">
+            <div className="w-full sm:mt-12 mt-4 mb-4 flex justify-between sm:mb-6">
+              <div className="sm:text-xl text-sm font-bold">
+                게시물{" "}
+                <span className="float-right ml-2 sm:text-xl text-sm font-bold text-primary">
+                  {cate === "전체"
+                    ? posts?.length
+                    : posts?.filter((post) => cate === post.type).length}
+                </span>
+              </div>
             </div>
-          </div>
 
-          <div className="w-full gap-4 bg-white grid grid-cols-2  sm:grid sm:grid-cols-3 sm:gap-6">
-            {cate === "전체"
-              ? posts
-                  ?.map((post: any) => (
-                    <PostCard key={post.postId} post={post} />
-                  ))
-                  .slice(0, 6 + slicenumber)
-              : posts
-                  ?.filter((post) => cate === post.type)
-                  .map((post: any) => (
-                    <PostCard key={post.postId} post={post} />
-                  ))
-                  .slice(0, 6 + slicenumber)}
-          </div>
-          {posts.length > slicenumber + 6 ? (
-            <button
-              className="sm:w-[120px] w-[348px] h-[50px] sm:h-[48px] mt-0 sm:mt-1 bg-transparent border border-primary text-primary hover:bg-second hover:border-none rounded-full hover:text-primary hover:font-bold"
-              onClick={() => setSliceNumber(slicenumber + 6)}
-            >
-              더보기
-            </button>
-          ) : (
-            <button
-              className="sm:w-[120px] w-[348px] h-[50px] sm:h-[48px] mt-0 sm:mt-1 bg-transparent border border-primary text-primary font-thin hover:bg-second hover:border-none rounded-3xl hover:text-primary hover:font-bold"
-              onClick={() => setSliceNumber(0)}
-            >
-              접기
-            </button>
-          )}
-
-          <div className="w-full mt-12 sm:mt-16 relative z-0">
-            <div className="sm:text-2xl text-xl font-bold mb-5 ml-3 sm:mb-3">
-              인기 많은 OHZU
-              <span className="text-primary">
-                {cate === "전체"
-                  ? userPosts?.length
-                  : userPosts?.filter((post) => cate === post.type).length}
-              </span>
+            <div className="w-full gap-4 bg-white grid grid-cols-2  sm:grid sm:grid-cols-3 sm:gap-6">
+              {cate === "전체"
+                ? posts
+                    ?.map((post: any) => (
+                      <PostCard key={post.postId} post={post} />
+                    ))
+                    .slice(0, 6 + slicenumber)
+                : posts
+                    ?.filter((post) => cate === post.type)
+                    .map((post: any) => (
+                      <PostCard key={post.postId} post={post} />
+                    ))
+                    .slice(0, 6 + slicenumber)}
             </div>
-            <div className="group">
-              <Swiper
-                spaceBetween={160}
-                slidesPerView={3}
-                scrollbar={{ draggable: true }}
-                navigation={{
-                  prevEl: navigationPrevRef.current,
-                  nextEl: navigationNextRef.current,
-                }}
-                breakpoints={{
-                  320: {
-                    slidesPerView: 3,
-                    spaceBetween: 130,
-                    slidesPerGroup: 2,
-                  },
-                  500: {
-                    slidesPerView: 3,
-                    spaceBetween: 30,
-                    slidesPerGroup: 3,
-                  },
-                  640: {
-                    slidesPerView: 3,
-                    spaceBetween: 30,
-                    slidesPerGroup: 3,
-                  },
-                  768: {
-                    slidesPerView: 3,
-                    spaceBetween: 24,
-                    slidesPerGroup: 3,
-                  },
-                }}
-                modules={[EffectCoverflow, Navigation, Mousewheel]}
-                grabCursor={true}
-                slidesPerGroup={2}
+            {posts.length > slicenumber + 6 ? (
+              <button
+                className="sm:w-[120px] w-[348px] h-[50px] sm:h-[48px] mt-0 sm:mt-1 bg-transparent border border-primary text-primary hover:bg-second hover:border-none rounded-full hover:text-primary hover:font-bold"
+                onClick={() => setSliceNumber(slicenumber + 6)}
               >
-                <div className="">
-                  {userLikePosts?.map((post: any) => (
-                    <SwiperSlide key={post.postId}>
-                      <PostCard key={post.postId} post={post} type="like" />
-                    </SwiperSlide>
-                  ))}{" "}
-                </div>
-                <div className="">
-                  <button
-                    ref={navigationPrevRef}
-                    className="w-[30px] h-[30px] translate-x-[4px] -translate-y-[70px] top-1/2 absolute p-1.5 hidden sm:group-hover:block hover:text-primary hover:bg-second/70 sm:w-[40px] sm:h-[40px] bg-black/20 text-white cursor-pointer sm:translate-x-[15px] sm:translate-y-[-30px] z-20 sm:top-[190px] rounded-full"
-                  >
-                    <BsChevronLeft
-                      size={20}
-                      className="relative right-[2px] bottom-[1px] sm:right-[-3px] sm:bottom-0"
-                    />
-                  </button>
-                </div>
-                <div className="">
-                  <button
-                    ref={navigationNextRef}
-                    className="w-[30px] h-[30px] translate-x-[340px] -translate-y-[70px] top-1/2 p-2 absolute hidden sm:group-hover:block hover:text-primary hover:bg-second/70 sm:w-[40px] sm:h-[40px] bg-black/20 text-white cursor-pointer sm:translate-x-[1110px] sm:translate-y-[-95px] z-20 sm:top-[260px] rounded-full"
-                  >
-                    <BsChevronRight
-                      size={20}
-                      className="relative right-[1px] bottom-[3px] sm:right-[-4px] sm:bottom-0"
-                    />
-                  </button>
-                </div>
-              </Swiper>
-            </div>
-          </div>
-
-          <div className="w-full mt-10 sm:mt-[90px] relative z-0 overflow-hidden">
-            <div className="sm:text-2xl text-xl font-bold mb-5 ml-3 sm:mb-3">
-              많이 본 OHZU
-              <span className="text-primary">
-                {cate === "전체"
-                  ? userPosts?.length
-                  : userPosts?.filter((post) => cate === post.type).length}
-              </span>
-            </div>
-            <div className="group overflow-visible">
-              <Swiper
-                spaceBetween={160}
-                slidesPerView={3}
-                scrollbar={{ draggable: true }}
-                navigation={{
-                  prevEl: prevRef.current,
-                  nextEl: nextRef.current,
-                }}
-                breakpoints={{
-                  320: {
-                    slidesPerView: 3,
-                    spaceBetween: 130,
-                    slidesPerGroup: 2,
-                  },
-                  500: {
-                    slidesPerView: 3,
-                    spaceBetween: 30,
-                    slidesPerGroup: 3,
-                  },
-                  640: {
-                    slidesPerView: 3,
-                    spaceBetween: 30,
-                    slidesPerGroup: 3,
-                  },
-                  768: {
-                    slidesPerView: 3,
-                    spaceBetween: 24,
-                    slidesPerGroup: 3,
-                  },
-                }}
-                modules={[EffectCoverflow, Navigation, Mousewheel]}
-                grabCursor={true}
-                slidesPerGroup={2}
+                더보기
+              </button>
+            ) : (
+              <button
+                className="sm:w-[120px] w-[348px] h-[50px] sm:h-[48px] mt-0 sm:mt-1 bg-transparent border border-primary text-primary font-thin hover:bg-second hover:border-none rounded-3xl hover:text-primary hover:font-bold"
+                onClick={() => setSliceNumber(0)}
               >
-                <div>
-                  {userViewPosts?.map((post: any) => (
-                    <SwiperSlide key={post.postId}>
-                      <PostCard key={post.postId} post={post} type="like" />
-                    </SwiperSlide>
-                  ))}{" "}
-                </div>
-                <div>
-                  <button
-                    ref={prevRef}
-                    className="hidden w-[30px] h-[30px] translate-x-[4px] -translate-y-[70px] top-1/2 absolute p-1.5 sm:group-hover:block hover:text-primary hover:bg-second/70 sm:w-[40px] sm:h-[40px] bg-black/20 text-white cursor-pointer sm:translate-x-[20px] sm:translate-y-[-30px] z-20 sm:top-[210px] rounded-full"
-                  >
-                    <BsChevronLeft
-                      size={20}
-                      className="relative sm:right-[-3px] sm:bottom-0 right-[2px] bottom-[1px]"
-                    />
-                  </button>
-                </div>
-                <div>
-                  <button
-                    ref={nextRef}
-                    className="w-[30px] h-[30px] translate-x-[340px] -translate-y-[70px] top-1/2 absolute p-2 hidden sm:group-hover:block hover:text-primary hover:bg-second/70 sm:w-[40px] sm:h-[40px] bg-black/20 text-white cursor-pointer sm:translate-x-[1110px] sm:translate-y-[-115px] z-20 sm:top-[300px] rounded-full"
-                  >
-                    <BsChevronRight
-                      size={20}
-                      className="relative sm:right-[-4px] sm:bottom-0 right-[1px] bottom-[3px]"
-                    />
-                  </button>
-                </div>
-              </Swiper>
-              <div></div>
+                접기
+              </button>
+            )}
+
+            <div className="w-full mt-12 sm:mt-16 relative z-0">
+              <div className="sm:text-2xl text-xl font-bold mb-5 ml-3 sm:mb-3">
+                인기 많은 OHZU
+                <span className="text-primary">
+                  {cate === "전체"
+                    ? userPosts?.length
+                    : userPosts?.filter((post) => cate === post.type).length}
+                </span>
+              </div>
+              <div className="group">
+                <Swiper
+                  spaceBetween={160}
+                  slidesPerView={3}
+                  scrollbar={{ draggable: true }}
+                  navigation={{
+                    prevEl: navigationPrevRef.current,
+                    nextEl: navigationNextRef.current,
+                  }}
+                  breakpoints={{
+                    320: {
+                      slidesPerView: 3,
+                      spaceBetween: 130,
+                      slidesPerGroup: 2,
+                    },
+                    500: {
+                      slidesPerView: 3,
+                      spaceBetween: 30,
+                      slidesPerGroup: 3,
+                    },
+                    640: {
+                      slidesPerView: 3,
+                      spaceBetween: 30,
+                      slidesPerGroup: 3,
+                    },
+                    768: {
+                      slidesPerView: 3,
+                      spaceBetween: 24,
+                      slidesPerGroup: 3,
+                    },
+                  }}
+                  modules={[EffectCoverflow, Navigation, Mousewheel]}
+                  grabCursor={true}
+                  slidesPerGroup={2}
+                >
+                  <div className="">
+                    {userLikePosts?.map((post: any) => (
+                      <SwiperSlide key={post.postId}>
+                        <PostCard key={post.postId} post={post} type="like" />
+                      </SwiperSlide>
+                    ))}{" "}
+                  </div>
+                  <div className="">
+                    <button
+                      ref={navigationPrevRef}
+                      className="w-[30px] h-[30px] translate-x-[4px] -translate-y-[70px] top-1/2 absolute p-1.5 hidden sm:group-hover:block hover:text-primary hover:bg-second/70 sm:w-[40px] sm:h-[40px] bg-black/20 text-white cursor-pointer sm:translate-x-[15px] sm:translate-y-[-30px] z-20 sm:top-[190px] rounded-full"
+                    >
+                      <BsChevronLeft
+                        size={20}
+                        className="relative right-[2px] bottom-[1px] sm:right-[-3px] sm:bottom-0"
+                      />
+                    </button>
+                  </div>
+                  <div className="">
+                    <button
+                      ref={navigationNextRef}
+                      className="w-[30px] h-[30px] translate-x-[340px] -translate-y-[70px] top-1/2 p-2 absolute hidden sm:group-hover:block hover:text-primary hover:bg-second/70 sm:w-[40px] sm:h-[40px] bg-black/20 text-white cursor-pointer sm:translate-x-[1110px] sm:translate-y-[-95px] z-20 sm:top-[260px] rounded-full"
+                    >
+                      <BsChevronRight
+                        size={20}
+                        className="relative right-[1px] bottom-[3px] sm:right-[-4px] sm:bottom-0"
+                      />
+                    </button>
+                  </div>
+                </Swiper>
+              </div>
+            </div>
+
+            <div className="w-full mt-10 sm:mt-[90px] relative z-0 overflow-hidden">
+              <div className="sm:text-2xl text-xl font-bold mb-5 ml-3 sm:mb-3">
+                많이 본 OHZU
+                <span className="text-primary">
+                  {cate === "전체"
+                    ? userPosts?.length
+                    : userPosts?.filter((post) => cate === post.type).length}
+                </span>
+              </div>
+              <div className="group overflow-visible">
+                <Swiper
+                  spaceBetween={160}
+                  slidesPerView={3}
+                  scrollbar={{ draggable: true }}
+                  navigation={{
+                    prevEl: prevRef.current,
+                    nextEl: nextRef.current,
+                  }}
+                  breakpoints={{
+                    320: {
+                      slidesPerView: 3,
+                      spaceBetween: 130,
+                      slidesPerGroup: 2,
+                    },
+                    500: {
+                      slidesPerView: 3,
+                      spaceBetween: 30,
+                      slidesPerGroup: 3,
+                    },
+                    640: {
+                      slidesPerView: 3,
+                      spaceBetween: 30,
+                      slidesPerGroup: 3,
+                    },
+                    768: {
+                      slidesPerView: 3,
+                      spaceBetween: 24,
+                      slidesPerGroup: 3,
+                    },
+                  }}
+                  modules={[EffectCoverflow, Navigation, Mousewheel]}
+                  grabCursor={true}
+                  slidesPerGroup={2}
+                >
+                  <div>
+                    {userViewPosts?.map((post: any) => (
+                      <SwiperSlide key={post.postId}>
+                        <PostCard key={post.postId} post={post} type="like" />
+                      </SwiperSlide>
+                    ))}{" "}
+                  </div>
+                  <div>
+                    <button
+                      ref={prevRef}
+                      className="hidden w-[30px] h-[30px] translate-x-[4px] -translate-y-[70px] top-1/2 absolute p-1.5 sm:group-hover:block hover:text-primary hover:bg-second/70 sm:w-[40px] sm:h-[40px] bg-black/20 text-white cursor-pointer sm:translate-x-[20px] sm:translate-y-[-30px] z-20 sm:top-[210px] rounded-full"
+                    >
+                      <BsChevronLeft
+                        size={20}
+                        className="relative sm:right-[-3px] sm:bottom-0 right-[2px] bottom-[1px]"
+                      />
+                    </button>
+                  </div>
+                  <div>
+                    <button
+                      ref={nextRef}
+                      className="w-[30px] h-[30px] translate-x-[340px] -translate-y-[70px] top-1/2 absolute p-2 hidden sm:group-hover:block hover:text-primary hover:bg-second/70 sm:w-[40px] sm:h-[40px] bg-black/20 text-white cursor-pointer sm:translate-x-[1110px] sm:translate-y-[-115px] z-20 sm:top-[300px] rounded-full"
+                    >
+                      <BsChevronRight
+                        size={20}
+                        className="relative sm:right-[-4px] sm:bottom-0 right-[1px] bottom-[3px]"
+                      />
+                    </button>
+                  </div>
+                </Swiper>
+                <div></div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
