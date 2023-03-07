49ed67ae0ed501d458c179a781bae401fb68d2b8
hwuiinn <gnldls8406@gmail.com>
2023-03-07 18:07:53 +0900

    🐛 Fix : Landing page UI
    - absolute 속성을 제거하여 균일한 레이아웃을 적용합니다.
    
    by @Hwuiinn

--- components/landing/landing_page.tsx
+++ components/landing/landing_page.tsx
@@ -7,22 +7,15 @@ import lottieJson from "../../animation/landing_page.json";
 const LandingPage = () => {
   return (
     <>
-      <div className="w-screen h-screen flex flex-col items-center justify-center relative">
-        <Image
-          className="w-screen h-screen absolute top-0 left-0"
-          src="/image/landing_background.jpg"
-          alt="렌딩페이지 배경화면"
-          width="200"
-          height="200"
-        />
+      <div className="w-screen h-screen bg-landing_bg bg-cover flex flex-col items-center justify-center">
         <Lottie
           loop
           animationData={lottieJson}
           play
-          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 lg:w-1/3 sm:w-1/2 "
+          className="sm:mb-[150px] mb-12 w-[70%]  sm:w-auto"
         />
-        <div className="absolute bottom-[17%] left-1/2 transform -translate-x-1/2 z-50">
-          <p className="sm:mb-5 mb-3 sm:text-base text-[10px] text-center font-semibold text-[#fff] ">
+        <div className="z-50">
+          <p className="sm:mb-5 mb-3 sm:text-base text-xs text-center font-semibold text-[#fff] ">
             나만의 혼합주를 사람들과 공유해보세요!
           </p>
           <div className="flex justify-center items-center sm:w-[280px] w-[160px] sm:h-[71px] h-10 m-auto text-sm sm:text-[36px]  text-white border-[1px] rounded-[50px] cursor-pointer duration-300  hover:bg-white/30 ">
